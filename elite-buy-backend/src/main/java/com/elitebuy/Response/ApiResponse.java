adf1baed699c3eb2f67b2bed93931bac4cbfeb5f
anshjain-24 <anshjain2912@gmail.com>
2024-03-22 09:32:54 +0530

    Started working on controller, Admin product controller done..

---
+++ elite-buy-backend/src/main/java/com/elitebuy/Response/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.elitebuy.Response;
+
+public class ApiResponse {
+    private String message;
+
+    private Boolean status;
+
+    public ApiResponse() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
