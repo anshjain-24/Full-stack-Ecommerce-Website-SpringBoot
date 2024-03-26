d18cc62904b43f565ac7703545ddb42d639d5149
anshjain-24 <anshjain2912@gmail.com>
2024-03-26 23:13:19 +0530

    product api integrated

--- elitbuy-frontend/src/State/Store.js
+++ elitbuy-frontend/src/State/Store.js
@@ -1,10 +1,12 @@
 import { legacy_createStore ,applyMiddleware, combineReducers } from 'redux';
 import {thunk} from 'redux-thunk';
 import { authReducer } from './Auth/Reducer';
+import { customerProductReducer } from './Product/Reducer';
 
 
 const rootReducers = combineReducers({
-        auth:authReducer
+        auth:authReducer,
+        product:customerProductReducer
 })
 
 export const store = legacy_createStore(rootReducers,applyMiddleware(thunk));
